fb12056c0691124c8c5ce42ed0586e1404acfa8a
Sasha <F712@rambler.ru>
2022-09-20 13:51:55 +0300

    Задача 41

--- Program.cs
+++ Program.cs
@@ -5,29 +5,40 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-
-
-
-// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
-// заданных уравнениями 
-// y = k1 * x + b1,
-// y = k2 * x + b2; 
-// значения b1, k1, b2 и k2 задаются пользователем.
-// b1 = 2, k1 = 5,
-// b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
-double DataEntry(string message)
+int M = 8;
+Console.WriteLine($"Введите {M} чисел: ");
+int count = 0;
+for (int i = 1; i <= M; i++)
 {
-    System.Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
+    Console.Write($"Введите число {i}: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++;
 }
-double b1 = DataEntry ("Введите значение b1 : ");
-double k1 = DataEntry ("Введите значение k1 : ");
-double b2 = DataEntry ("Введите значение b2 : ");
-double k2 = DataEntry ("Введите значение k2 : ");
+Console.WriteLine($"Положительных чисел: {count}");
+
+// // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// // заданных уравнениями 
+// // y = k1 * x + b1,
+// // y = k2 * x + b2; 
+// // значения b1, k1, b2 и k2 задаются пользователем.
+// // b1 = 2, k1 = 5,
+// // b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// double DataEntry(string message)
+// {
+//     System.Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+// double b1 = DataEntry ("Введите значение b1 : ");
+// double k1 = DataEntry ("Введите значение k1 : ");
+// double b2 = DataEntry ("Введите значение b2 : ");
+// double k2 = DataEntry ("Введите значение k2 : ");
 
 
-double x = (-b2 + b1)/(-k1 + k2);
-double y = k2 * x + b2;
+// double x = (-b2 + b1)/(-k1 + k2);
+// double y = k2 * x + b2;
 
-Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+// Console.WriteLine($"Две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
+
+// //End
